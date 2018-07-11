a97e400cd3f96738538ddc527ccc72a7dc083469
jonatasgeofficial <jonatasge@gmail.com>
2018-07-10 21:13:30 -0300

    end

--- views/js/main.js
+++ views/js/main.js
@@ -26,9 +26,32 @@ function deleteEvento(id) {
     $.ajax({
         url: '/delete',
         type: 'post',
-        data: { id: id },
+        data: {
+            id: id
+        },
         success: function () {
             location.reload();
         }
     });
 }
+
+function updateEvento(ano, mes, dia, evento, id) {
+    $('input[name="id"]').val(id);
+    $('input[name="ano"]').val(ano);
+    $('input[name="mes"]').val(mes);
+    $('input[name="dia"]').val(dia);
+    $('input[name="evento"]').val(evento);
+    $('#modal1').modal('open');
+}
+
+function cancel() {
+    $('form').trigger("reset");
+}
+
+$(document).ready(function () {
+    $('.modal').modal();
+    $('.modal-trigger').on('click', function () {
+        $('input[name="id"]').val('');
+        cancel();
+    });
+});
